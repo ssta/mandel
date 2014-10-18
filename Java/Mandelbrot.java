a50334ebb325b7a2d7fdb7730ada470d98f9550c
Stephen Stafford <clothcat@gmail.com>
2014-10-18 19:58:14 +0100

    initial import

---
+++ Java/Mandelbrot.java
@@ -0,0 +1,44 @@
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.swing.JFrame;
+ 
+public class Mandelbrot extends JFrame {
+ 
+    private final int MAX_ITER = 1000;
+    private final double ZOOM = 200;
+    private final BufferedImage I;
+    private double zx, zy, cX, cY, tmp;
+    private double BAILOUT=16;
+ 
+    public Mandelbrot() {
+        super("Mandelbrot Set");
+        setBounds(100, 100, 800, 600);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        for (int y = 0; y < getHeight(); y++) {
+            for (int x = 0; x < getWidth(); x++) {
+                zx = zy = 0;
+                cX = (x - 400) / ZOOM;
+                cY = (y - 300) / ZOOM;
+                int iter = MAX_ITER;
+                while (zx * zx + zy * zy < BAILOUT && iter > 0) {
+                    tmp = zx * zx - zy * zy + cX;
+                    zy = 2.0 * zx * zy + cY;
+                    zx = tmp;
+                    iter--;
+                }
+                I.setRGB(x, y, iter | (iter << 8));
+            }
+        }
+    }
+ 
+    @Override
+    public void paint(Graphics g) {
+        g.drawImage(I, 0, 0, this);
+    }
+ 
+    public static void main(String[] args) {
+        new Mandelbrot().setVisible(true);
+    }
+}
